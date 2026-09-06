95fc0dd7458e4479275793b5478ab7302afc209b
margauxxs <margauxsteve9@gmail.com>
2026-09-07 07:16:15 +0800

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,33 +1,31 @@
-let i = 0;
-// Personalized typewriter text for Mahal
-let text1 = "Happy 1st Anniversary, Mahal!";
-let text2 = "Welcome to our special memory page. ❤️";
-let speed = 100;
+// Universal Typewriter Engine for Anniversary Site
+var str = "Hi Mahal! Happy 1st Anniversary! ❤️";
+var str2 = "I created a little collection of our memories together. Click the heart below! ✨";
 
-function typeWriter(text, para){
-	if(ok == 2){
-		clearInterval(typeInterval);
-	}
-	if(i < text.length){
-		document.getElementById(para).innerHTML += text.charAt(i);
-		i++;
-		speed = Math.random() * 50 + 100;
-	}
-	else{
-		if(ok == 0){
-			i = 0;
-		}
-		ok += 1;
-	}
+var i = 0;
+var j = 0;
+
+function typeWriter1() {
+    var div1 = document.getElementById("txt1");
+    if (div1 && i < str.length) {
+        div1.innerHTML += str.charAt(i);
+        i++;
+        setTimeout(typeWriter1, 80); // Typing speed for first line
+    } else {
+        setTimeout(typeWriter2, 400); // Pause before second line starts
+    }
 }
 
-var typeInterval;
+function typeWriter2() {
+    var div2 = document.getElementById("txt2");
+    if (div2 && j < str2.length) {
+        div2.innerHTML += str2.charAt(j);
+        j++;
+        setTimeout(typeWriter2, 60); // Typing speed for second line
+    }
+}
 
-typeInterval = setInterval(function(){
-	if(ok == 0){
-		typeWriter(text1, "txt1");
-	}
-	else if(ok == 1){
-		typeWriter(text2, "txt2");
-	}
-}, 100);
+// Automatically start typing as soon as the page loads
+window.addEventListener("DOMContentLoaded", function() {
+    setTimeout(typeWriter1, 500);
+});
